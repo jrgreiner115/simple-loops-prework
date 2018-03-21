c57d8f7017b3888be94f87f250e1d3e164b60dc3
Joe Greiner <jrgreiner115@gmail.com>
2018-03-21 23:10:38 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,8 @@ end
 def times_iterator(number_of_times)
   counter = 1
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times.times
+  number_of_times.times do
+    puts phrase
 end
 end
 
